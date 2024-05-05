a5cf94bcdc67ec17f157596d3201d6ea18e71bb5
Sarausap <151543470+Sarausap@users.noreply.github.com>
2024-05-05 18:02:56 +0800

    added calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+import 'package:room_utilization/calendar.dart'; // Ensure this import is correct
 
 void main() {
   runApp(const MyApp());
@@ -11,68 +12,99 @@ const cellGrey = Color(0xffcfd4e0);
 const cellBlue = Color(0xff1553be);
 const background = Color.fromARGB(0, 36, 40, 48);
 
-class PietPainting extends StatelessWidget {
+class PietPainting extends StatefulWidget {
   const PietPainting({Key? key}) : super(key: key);
 
   @override
+  _PietPaintingState createState() => _PietPaintingState();
+}
+
+class _PietPaintingState extends State<PietPainting> {
+  String path = 'assets/images/cisc2ndfloorv2.png';
+
+  void SwitchFloor(int floor) {
+    setState(() {
+      if (floor == 1) {
+        path = 'assets/images/cisck2ndfloor.png';
+      } else {
+        path = 'assets/images/cisc2ndfloorv2.png';
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    final columnSizes = screenSize.width > 600
+        ? [2.0.fr, 2.0.fr, 1.fr, 3.fr, 3.fr]
+        : [10.0.fr, 10.0.fr];
+    final rowSizes = screenSize.width > 600
+        ? [.5.fr, 3.0.fr, 3.0.fr, .5.fr]
+        : [.5.fr, 1.0.fr, 1.0.fr, .5.fr, 1.0.fr, .5.fr];
+    final areas = screenSize.width > 600
+        ? '''
+        hd hd hd hd hd
+        cf2 cf2 fl ca ca
+        cf2 cf2 fl ca ca
+        fo fo fo fo fo
+        '''
+        : '''
+        hd hd
+        cf2 cf2
+        cf2 cf2
+        fl fl
+        ca ca
+        fo fo
+        ''';
+
     return Container(
       color: background,
       child: LayoutGrid(
-        columnGap: 2,
+        columnGap: 0,
         rowGap: 2,
-        areas: '''
-          w w g g
-          cf2 cf2 g g
-          cf2 cf2 g g
-          ww ww g g
-        ''',
-        // A number of extension methods are provided for concise track sizing
-        columnSizes: [1.0.fr, 1.0.fr, 1.0.fr, 2.2.fr],
-        rowSizes: [
-          .5.fr,
-          1.0.fr,
-          1.0.fr,
-          .5.fr
-        ],
+        areas: areas,
+        columnSizes: columnSizes,
+        rowSizes: rowSizes,
         children: [
-          // Column 1
-          gridArea('r').containing(Container(color: cellRed)),
-          gridArea('y').containing(Container(color: cellMustard)),
-          // Column 2
-          gridArea('R').containing(Container(color: cellRed)),
-          // Column 3
-          gridArea('B').containing(Container(color: cellBlue)),
-          gridArea('Y').containing(Container(color: cellMustard)),
-          gridArea('g').containing(Container(color: cellGrey)),
-          // Column 4
-          gridArea('b').containing(Container(color: cellBlue)),
-          // Column 5
-          gridArea('yy').containing(Container(color: cellMustard)),
-          //new
-          gridArea('g').containing(Container(color: Colors.green)),
-          gridArea('w').containing
-            (Container(
-              color: Colors.white,
-              child: Container(
-                alignment: AlignmentDirectional.center,
-                child: Text("CISC 2nd Floor",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold
-                  ),),
-              ),)),
-          gridArea('ww').containing(Container(color: Colors.white)),
-
-          //image
+          gridArea('ca').containing(CalendarWidget()),
+          gridArea('hd').containing(Container(
+            color: Colors.white,
+            child: Container(
+              alignment: AlignmentDirectional.center,
+              child: Text(
+                "CISC 2nd Floor",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          )),
+          gridArea('hd').containing(Container(color: Colors.white)),
+          gridArea('fl').containing(Container(
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    SwitchFloor(1);
+                  },
+                  child: Text("1"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    SwitchFloor(2);
+                  },
+                  child: Text("2"),
+                ),
+              ],
+            ),
+          )),
           gridArea('cf2').containing(
             Stack(
               children: [
-                Image.asset('assets/images/cisc2ndfloorv2.png'),
-                
+                Image.asset(path),
               ],
             ),
           ),
+          gridArea('fo').containing(Container(color: Colors.white)),
         ],
       ),
     );
@@ -84,11 +116,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WidgetsApp(
+    return MaterialApp(
       title: 'Layout Grid Desktop Example',
       debugShowCheckedModeBanner: false,
-      color: Colors.white,
-      builder: (context, child) => const PietPainting(),
+      home: const PietPainting(),
     );
   }
 }
